c62a843ea16cf1253417ae3f5513b3f663118e84
WakWakBird <134926267+WakWakBird@users.noreply.github.com>
2023-07-28 22:08:22 +0900

    feat : 컴퓨터 개수 인식 클래스로 변경

--- test.py
+++ test.py
@@ -3,14 +3,13 @@ import numpy as np
 
 
 class Webcam:
-  
-    def __init__(self,index):
+    def __init__(self, index):
         self.index = index
 
-    def count_connected_cameras():
+    def count_connected_cameras(self):
         num_cameras = 0
-        index = 0
-        while True : 
+        index = self.index
+        while True:
             capture = cv2.VideoCapture(index)
             if not capture.isOpened():
                 break
@@ -18,6 +17,7 @@ class Webcam:
             capture.release()
             index += 1
         return num_cameras
-    
-    num_cameras = count_connected_cameras()
-    print(num_cameras)
+
+webcam = Webcam(0)  # Webcam 클래스의 인스턴스를 생성하고, index 값으로 0을 전달하여 초기화
+num_cameras = webcam.count_connected_cameras()
+print(f"컴퓨터에 연결된 카메라 개수: {num_cameras}개")
