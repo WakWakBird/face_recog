3a7dfa2a59d028389981dd07cf9a38904c5adac6
WakWakBird <134926267+WakWakBird@users.noreply.github.com>
2023-07-28 21:45:54 +0900

    feat : 테스트 파일에 카메라 개수 인식코드 추가

--- test.py
+++ test.py
@@ -1,24 +1,23 @@
-import os
-import face_recognition
-
-class FaceMatcher:
-    def __init__(self, image_filename):
-        self.image_to_be_matched = face_recognition.load_image_file(image_filename)
-        self.image_to_be_matched_encoded = face_recognition.face_encodings(self.image_to_be_matched)[0]
-        self.images = os.listdir('images')
-
-    def match_faces(self):
-        for image in self.images:
-            current_image = face_recognition.load_image_file("images/" + image)
-            current_image_encoded = face_recognition.face_encodings(current_image)[0]
-            result = face_recognition.compare_faces([self.image_to_be_matched_encoded], current_image_encoded)
-            if result[0]:
-                print("Matched: " + image)
-            else:
-                print("Not matched: " + image)
-
-if __name__ == "__main__":
-    face_matcher = FaceMatcher('my_image.jpg')
-    face_matcher.match_faces()
+import cv2
+import numpy as np
 
 
+class Webcam:
+  
+    def __init__(self,index):
+        self.index = index
+
+    def count_connected_cameras():
+        num_cameras = 0
+        index = 0
+        while True : 
+            capture = cv2.VideoCapture(index)
+            if not capture.isOpened():
+                break
+            num_cameras += 1
+            capture.release()
+            index += 1
+        return num_cameras
+    
+    num_cameras = count_connected_cameras()
+    print(num_cameras)
