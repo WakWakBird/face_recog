16d70c0389c17745e5e6e87d49a9a944796254d7
WakWakBird <134926267+WakWakBird@users.noreply.github.com>
2023-08-09 02:11:37 +0900

    add:all

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import cv2
-import numpy
+from classfrineds import *
 
+cameratracer = CameraTracer(0)
+
+num_camera = cameratracer.count_connected_cameras()
+print(f"컴퓨터에 연결된 카메라 개수: {num_camera}개")
+
+user_input = int(input("지정할 카메라를 입력하세요: "))
+cameratracer.camera_input(user_input)
+cameratracer.camera_designate()
+
+facepredict = FacePredict(user_input)
+facepredict.predict_faces()
