5be028bbd013d8eaed3f6930cd8863209757922c
WakWakBird <134926267+WakWakBird@users.noreply.github.com>
2023-08-11 14:13:51 +0900

    oo

--- classfriends.py
+++ classfriends.py
@@ -2,6 +2,35 @@ import face_recognition
 import numpy as np
 import cv2
 
+class CameraTracer:
+    def __init__(self, index):
+        self.index = index
+        self.num_cameras = 0
+        self.inputed_camera = 0
+        self.capture = 0
+
+    def count_connected_cameras(self):
+        index = self.index
+        while True:
+            self.capture = cv2.VideoCapture(index)
+            if not self.capture.isOpened():
+                break
+            self.num_cameras += 1
+            self.capture.release()
+            index += 1
+        return self.num_cameras 
+    
+    def camera_input(self, inputed_camera):
+        self.inputed_camera = inputed_camera
+
+    def camera_designate(self):
+        if self.inputed_camera <= self.num_cameras:
+            print(f"{self.inputed_camera - 1}번 카메라를 지정합니다.")
+        elif self.inputed_camera >= self.num_cameras:
+            print(f"{self.num_cameras + 1} 번 이상 카메라는 사용할 수 없습니다...") 
+            return False
+
+
 class FaceRecognizer:
     def __init__(self):
         self.known_face_encodings = []
@@ -31,40 +60,17 @@ class FaceRecognizer:
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+            print(f"Detected face: {name}, Location: ({top}, {right}, {bottom}, {left})")
 
-class CameraTracer:
-    def __init__(self, index):
-        self.index = index
-        self.num_cameras = 0
-        self.inputed_camera = 0
-        self.capture = 0
-
-    def count_connected_cameras(self):
-        index = self.index
-        while True:
-            self.capture = cv2.VideoCapture(index)
-            if not self.capture.isOpened():
-                break
-            self.num_cameras += 1
-            self.capture.release()
-            index += 1
-        return self.num_cameras 
-    
-    def camera_input(self, inputed_camera):
-        self.inputed_camera = inputed_camera
-
-    def camera_designate(self):
-        if self.inputed_camera <= self.num_cameras:
-            print(f"{self.inputed_camera - 1}번 카메라를 지정합니다.")
-        elif self.inputed_camera >= self.num_cameras:
-            print(f"{self.num_cameras + 1} 번 이상 카메라는 사용할 수 없습니다...") 
-            return False
 
 class FacePredict:
-    def __init__(self, user_input):
+    def __init__(self, user_input, *known_faces):
         self.tracer = CameraTracer(0)
         self.tracer.capture = cv2.VideoCapture(user_input - 1)
         self.recognizer = FaceRecognizer()
+
+        if known_faces:
+            self.recognizer.load_known_faces(known_faces)
 
     def predict_faces(self):
         while True:
@@ -73,7 +79,6 @@ class FacePredict:
                 break
 
             self.recognizer.recognize_faces(frame)
-
             cv2.imshow('Video', frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -82,16 +87,4 @@ class FacePredict:
         self.tracer.capture.release()
         cv2.destroyAllWindows()
 
-if __name__ == "__main__":
-    cameratracer = CameraTracer(0)
-
-    num_camera = cameratracer.count_connected_cameras()
-    print(f"컴퓨터에 연결된 카메라 개수: {num_camera}개")
-
-    user_input = int(input("지정할 카메라를 입력하세요: "))
-    cameratracer.camera_input(user_input)
-    cameratracer.camera_designate()
-
-    facepredict = FacePredict(user_input)
-    facepredict.predict_faces()
 
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from classfrineds import *
+from classfriends import *
 
 cameratracer = CameraTracer(0)
-
 num_camera = cameratracer.count_connected_cameras()
 print(f"컴퓨터에 연결된 카메라 개수: {num_camera}개")
-
 user_input = int(input("지정할 카메라를 입력하세요: "))
 cameratracer.camera_input(user_input)
 cameratracer.camera_designate()
 
-facepredict = FacePredict(user_input)
+
+facepredict = FacePredict(user_input,
+                              ("KIM.jpg", "Min-Jae Kim"),
+                              ("SON.jpg","Heung-Min Son"),
+                              ("CR7.jpg","Cristiano_Ronaldo"),
+                              ("Emerson.jpg","Emerson_Royal"),
+                              ("Karim_Benzema.jpg","Karim_Benzema"),
+                              ("Harry_Kane.jpg","Harry_Kane"),
+                              ("GOAT.jpg","Lionel_Messi"),
+                              ("gay.jpg","Hyeon-Yup Jeon")
+                              )
 facepredict.predict_faces()
